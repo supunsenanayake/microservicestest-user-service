889439ebe258a842a19d1198794088490bf93855
Supun <supunsnnk@gmail.com>
2025-04-06 21:58:35 +0800

    init commit

---
+++ src/test/java/User/Service/Microservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package User.Service.Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
